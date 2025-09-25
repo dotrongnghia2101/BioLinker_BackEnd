c68f147cb60412823004c48f6b0e92907596478e
dotrongnghia2101 <dotrongnghia2101@gmail.com>
2025-09-25 21:34:13 +0700

    add update profile dto

---
+++ BioLinker/DTO/UpdateProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace BioLinker.DTO
+{
+    public class UpdateProfile
+    {
+        public string UserId { get; set; }
+        public string? FullName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserImage { get; set; }
+    }
+}
