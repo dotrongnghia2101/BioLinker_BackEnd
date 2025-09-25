da7e05ddb25dbafc4ae8bd23c346d46ab446271a
dotrongnghia2101 <dotrongnghia2101@gmail.com>
2025-09-25 21:22:02 +0700

    update register and login after add role

---
+++ BioLinker/DTO/UpdateRole.cs
@@ -0,0 +1,8 @@
+﻿namespace BioLinker.DTO
+{
+    public class UpdateRole
+    {
+        public string UserId { get; set; }
+        public string NewRoleName { get; set; } // FreeUser, ProUser, Admin
+    }
+}
---
+++ BioLinker/Respository/IUserRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using BioLinker.Enities;
+
+namespace BioLinker.Respository
+{
+    public interface IUserRoleRepository
+    {
+        Task AddAsync(UserRole userRole);
+        Task<UserRole?> GetByIdAsync(string userRoleId);
+    }
+}
