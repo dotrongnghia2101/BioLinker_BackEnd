43730983275b2e61b3d5873922413f271e75d66c
dotrongnghia2101 <dotrongnghia2101@gmail.com>
2025-10-10 22:40:45 +0700

    add crud count template click api

---
+++ BioLinker/Respository/ClickRepo/ICountTemplateClickedRepository.cs
@@ -0,0 +1,16 @@
+﻿using BioLinker.Enities;
+
+namespace BioLinker.Respository.ClickRepo
+{
+    public interface ICountTemplateClickedRepository
+    {
+        Task<IEnumerable<CountTemplateClicked>> GetAllAsync();
+        Task<CountTemplateClicked?> GetByIdAsync(string id);
+        Task<IEnumerable<CountTemplateClicked>> GetByDateAsync(DateTime date);
+        Task<IEnumerable<CountTemplateClicked>> GetByWeekAsync(int week, int year);
+        Task<IEnumerable<CountTemplateClicked>> GetByMonthAsync(int month, int year);
+        Task<int> GetTotalCountAsync();
+        Task AddAsync(CountTemplateClicked entity);
+        Task DeleteAsync(string id);
+    }
+}
---
+++ BioLinker/Service/ICountTemplateClickedService.cs
@@ -0,0 +1,16 @@
+﻿using BioLinker.DTO.CountDTO;
+
+namespace BioLinker.Service
+{
+    public interface ICountTemplateClickedService
+    {
+        Task<IEnumerable<CountTemplateClickedResponse>> GetAllAsync();
+        Task<CountTemplateClickedResponse?> GetByIdAsync(string id);
+        Task<IEnumerable<CountTemplateClickedResponse>> GetByDateAsync(DateTime date);
+        Task<IEnumerable<CountTemplateClickedResponse>> GetByWeekAsync(int week, int year);
+        Task<IEnumerable<CountTemplateClickedResponse>> GetByMonthAsync(int month, int year);
+        Task<int> GetTotalCountAsync();
+        Task<CountTemplateClickedResponse> AddAsync(CountTemplateClickedCreated dto);
+        Task DeleteAsync(string id);
+    }
+}
