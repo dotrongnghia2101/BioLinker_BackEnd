146515854920b76a53998f425b23c934386da399
dotrongnghia2101 <dotrongnghia2101@gmail.com>
2025-10-18 21:34:28 +0700

    add crud link api

---
+++ BioLinker/Service/ILinkService.cs
@@ -0,0 +1,14 @@
+﻿using BioLinker.DTO.LinkDTO;
+using BioLinker.Enities;
+
+namespace BioLinker.Service
+{
+    public interface ILinkService
+    {
+        Task<IEnumerable<LinkResponse?>> GetAllByBioPageAsync(string bioPageId);
+        Task<LinkResponse?> GetByIdAsync(string linkId);
+        Task<LinkResponse?> CreateAsync(LinkCreate link);
+        Task UpdateAsync(Link link);
+        Task DeleteAsync(string linkId);
+    }
+}
